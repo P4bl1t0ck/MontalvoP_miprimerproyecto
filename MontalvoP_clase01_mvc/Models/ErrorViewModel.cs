4afa1dbcd649fc434f37f078cc78b575e7395103
Pablo.Montalvo <pablo.montalvo.marroquin@udla.edu.ec>
2025-04-13 23:11:19 -0500

    Agregar archivos de proyecto.

---
+++ MontalvoP_clase01_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MontalvoP_clase01_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
